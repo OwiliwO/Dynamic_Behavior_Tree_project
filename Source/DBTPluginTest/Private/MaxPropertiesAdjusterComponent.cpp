1db2081f7ec1ea65c8418ace731021e1502862e8
Makhonov Gleb <goldendor753@gmail.com>
2026-06-03 11:18:25 +1000

    Update main files

--- Source/DBTPluginTest/Private/MaxPropertiesAdjusterComponent.cpp
+++ Source/DBTPluginTest/Private/MaxPropertiesAdjusterComponent.cpp
@@ -11,6 +11,8 @@
 #include "Serialization/JsonSerializer.h"
 #include "Engine/Engine.h"
 #include "DBTBehaviorTreeDataManager.h"
+#include "AIController.h"
+#include "EngineUtils.h"
 
 UMaxPropertiesAdjusterComponent::UMaxPropertiesAdjusterComponent()
 {
@@ -57,8 +59,6 @@ void UMaxPropertiesAdjusterComponent::ExecuteAdjustment()
 {
 	GLog->Logf(ELogVerbosity::Display, TEXT("MaxPropertiesAdjusterComponent: Starting adjustment..."));
 	ExecuteAdjustmentLogic();
-
-	// StartAdjustmentTimer();
 }
 
 void UMaxPropertiesAdjusterComponent::ExecuteAdjustmentLogic()
@@ -88,7 +88,7 @@ void UMaxPropertiesAdjusterComponent::ExecuteAdjustmentLogic()
 
     GLog->Logf(ELogVerbosity::Display, TEXT("ExecuteAdjustment: Found %d PlayerController(s)"), PlayerControllers.Num());
 
-    FString ConfigFilePath = FPaths::ProjectConfigDir() / TEXT("AIControllerMaxValues.json");
+    FString ConfigFilePath = FPaths::ProjectConfigDir() / TEXT("GASCharacterMaxValues.json");
 
 	FString JsonString;
 	if (!FFileHelper::LoadFileToString(JsonString, *ConfigFilePath))
@@ -183,8 +183,7 @@ void UMaxPropertiesAdjusterComponent::ExecuteAdjustmentLogic()
 						}
 						FileValues.Add(FileValue);
 
-						GLog->Logf(ELogVerbosity::Display, TEXT("Found MAX property: %s (from class %s) | Current: %f | File: %f"),
-							*PropertyName, *CurrentClass->GetName(), CurrentValue, FileValue);
+						GLog->Logf(ELogVerbosity::Display, TEXT("Found MAX property: %s (from class %s) | Current: %f | File: %f"), *PropertyName, *CurrentClass->GetName(), CurrentValue, FileValue);
 					}
 				}
 			}
@@ -201,54 +200,205 @@ void UMaxPropertiesAdjusterComponent::ExecuteAdjustmentLogic()
 			continue;
 		}
 
-		double SumDifferences = 0.0;
-		int ValidPropertiesCount = 0;
+		double Coefficient = 1.0;
+		const double Beta = 3.0;
+		const double Epsilon = 1e-8;
+
+		TArray<double> LogRatios;
+		LogRatios.Reserve(MaxProperties.Num());
+
+		int32 ValidPropertiesCount = 0;
 
 		for (int32 i = 0; i < MaxProperties.Num(); i++)
 		{
-			double Difference = FMath::Abs(CurrentValues[i] - FileValues[i]);
 			if (FileValues[i] != 0.0)
 			{
-				SumDifferences += Difference;
+				double Ratio = CurrentValues[i] / (FileValues[i] + Epsilon);
+				double LogRatio = FMath::Loge(Ratio);
+				LogRatios.Add(LogRatio);
 				ValidPropertiesCount++;
 			}
 		}
 
-		double Coefficient = 1.0;
-		if (ValidPropertiesCount > 0 && SumDifferences > 0.0)
+		if (ValidPropertiesCount > 0)
 		{
-			Coefficient = SumDifferences / ValidPropertiesCount;
+			double SumLog = 0.0;
+			for (double LogVal : LogRatios)
+			{
+				SumLog += LogVal;
+			}
+			double MeanLog = SumLog / ValidPropertiesCount;
+
+			double SumSqDiff = 0.0;
+			for (double LogVal : LogRatios)
+			{
+				double Diff = LogVal - MeanLog;
+				SumSqDiff += Diff * Diff;
+			}
+			double Variance = SumSqDiff / ValidPropertiesCount;
+			double Sigma = FMath::Sqrt(Variance);
+
+			Coefficient = FMath::Exp(-Beta * Sigma);
 		}
 
 		GLog->Logf(ELogVerbosity::Display, TEXT("Properties found: %d, Valid for calculation: %d"), MaxProperties.Num(), ValidPropertiesCount);
-		GLog->Logf(ELogVerbosity::Display, TEXT("Sum of differences: %f, Coefficient: %f"), SumDifferences, Coefficient);
+		GLog->Logf(ELogVerbosity::Display, TEXT("Coefficient: %f"), Coefficient);
+
+		TArray<AAIController*> AIControllers;
+		for (TActorIterator<AAIController> It(World); It; ++It)
+		{
+			AAIController* AIController = *It;
+			if (AIController && AIController->IsValidLowLevel())
+			{
+				AIControllers.Add(AIController);
+			}
+		}
+
+		if (AIControllers.Num() == 0)
+		{
+			GLog->Logf(ELogVerbosity::Warning, TEXT("No AIControllers found in world"));
+			return;
+		}
+
+		GLog->Logf(ELogVerbosity::Display, TEXT("Found %d AIController(s) to adjust"), AIControllers.Num());
+
+		for (AAIController* AIController : AIControllers)
+		{
+			APawn* AIControlledPawn = AIController->GetPawn();
+			if (!AIControlledPawn)
+			{
+				GLog->Logf(ELogVerbosity::Warning, TEXT("AIController %s does not control any Pawn"), *AIController->GetName());
+				continue;
+			}
+
+			ACharacter* AIControlledCharacter = Cast<ACharacter>(AIControlledPawn);
+			if (!AIControlledCharacter)
+			{
+				GLog->Logf(ELogVerbosity::Warning, TEXT("AIController %s controls pawn that is not ACharacter"), *AIController->GetName());
+				continue;
+			}
+
+			GLog->Logf(ELogVerbosity::Display, TEXT("Processing AIController: %s, Character: %s"), *AIController->GetName(), *AIControlledCharacter->GetName());
+
+			UClass* AICharacterClass = AIControlledCharacter->GetClass();
+			TArray<FNumericProperty*> AIMaxProperties;
+			TArray<double> AICurrentValues;
+
+			for (UClass* CurrentClass = AICharacterClass; CurrentClass; CurrentClass = CurrentClass->GetSuperClass())
+			{
+				for (TFieldIterator<FProperty> PropIt(CurrentClass, EFieldIteratorFlags::ExcludeSuper); PropIt; ++PropIt)
+				{
+					FProperty* Property = *PropIt;
+					FString PropertyName = Property->GetName();
+
+					if (PropertyName.Contains(TEXT("Max")) && Property->IsA(FNumericProperty::StaticClass()))
+					{
+						FNumericProperty* NumericProperty = CastField<FNumericProperty>(Property);
+						if (NumericProperty)
+						{
+							bool bAlreadyAdded = false;
+							for (FNumericProperty* AddedProp : AIMaxProperties)
+							{
+								if (AddedProp->GetFName() == NumericProperty->GetFName())
+								{
+									bAlreadyAdded = true;
+									break;
+								}
+							}
+
+							if (bAlreadyAdded) continue;
+
+							AIMaxProperties.Add(NumericProperty);
+
+							void* PropertyValuePtr = NumericProperty->ContainerPtrToValuePtr<void>(AIControlledCharacter);
+							double CurrentValue = 0.0;
+
+							if (NumericProperty->IsFloatingPoint())
+							{
+								CurrentValue = NumericProperty->GetFloatingPointPropertyValue(PropertyValuePtr);
+							}
+							else if (NumericProperty->IsInteger())
+							{
+								try
+								{
+									CurrentValue = (double)NumericProperty->GetSignedIntPropertyValue(PropertyValuePtr);
+								}
+								catch (...)
+								{
+									CurrentValue = (double)NumericProperty->GetUnsignedIntPropertyValue(PropertyValuePtr);
+								}
+							}
+
+							AICurrentValues.Add(CurrentValue);
+						}
+					}
+				}
+
+				if (CurrentClass == ACharacter::StaticClass())
+				{
+					break;
+				}
+			}
+
+			if (AIMaxProperties.Num() == 0)
+			{
+				GLog->Logf(ELogVerbosity::Display, TEXT("No MAX properties found in AI character hierarchy"));
+				continue;
+			}
+
+			for (int32 i = 0; i < AIMaxProperties.Num(); i++)
+			{
+				FNumericProperty* NumericProperty = AIMaxProperties[i];
+				FString PropertyName = NumericProperty->GetName();
+
+				void* PropertyValuePtr = NumericProperty->ContainerPtrToValuePtr<void>(AIControlledCharacter);
+				double NewValue = AICurrentValues[i] * (1 + Coefficient);
+
+				if (NumericProperty->IsFloatingPoint())
+				{
+					NumericProperty->SetFloatingPointPropertyValue(PropertyValuePtr, NewValue);
+				}
+				else if (NumericProperty->IsInteger())
+				{
+					int64 IntValue = FMath::RoundToInt(NewValue);
+					NumericProperty->SetIntPropertyValue(PropertyValuePtr, IntValue);
+				}
+
+				GLog->Logf(ELogVerbosity::Display, TEXT("Adjusted %s in AI character: %f -> %f (Coefficient: %f)"), *PropertyName, AICurrentValues[i], NewValue, Coefficient);
+			}
+
+			GLog->Logf(ELogVerbosity::Display, TEXT("AIController %s: All MAX properties adjusted by coefficient %f"), *AIController->GetName(), Coefficient);
+		}
 
 		for (int32 i = 0; i < MaxProperties.Num(); i++)
 		{
 			FNumericProperty* NumericProperty = MaxProperties[i];
 			FString PropertyName = NumericProperty->GetName();
-
-			void* PropertyValuePtr = NumericProperty->ContainerPtrToValuePtr<void>(ControlledCharacter);
-			double NewValue = CurrentValues[i] * Coefficient;
-
-			if (NumericProperty->IsFloatingPoint())
-			{
-				NumericProperty->SetFloatingPointPropertyValue(PropertyValuePtr, NewValue);
-			}
-			else if (NumericProperty->IsInteger())
-			{
-				int64 IntValue = FMath::RoundToInt(NewValue);
-				NumericProperty->SetIntPropertyValue(PropertyValuePtr, IntValue);
-			}
-
-			GLog->Logf(ELogVerbosity::Display, TEXT("Adjusted %s: %f -> %f"), *PropertyName, CurrentValues[i], NewValue);
+			double CurrentValue = CurrentValues[i];
+			JsonObject->SetNumberField(PropertyName, CurrentValue);
 		}
 
-		GLog->Logf(ELogVerbosity::Display, TEXT("PlayerController %s: All MAX properties adjusted by coefficient %f"),
-			*PlayerController->GetName(), Coefficient);
+		FString OutputString;
+		TSharedRef<TJsonWriter<>> Writer = TJsonWriterFactory<>::Create(&OutputString);
+		if (FJsonSerializer::Serialize(JsonObject.ToSharedRef(), Writer))
+		{
+			if (FFileHelper::SaveStringToFile(OutputString, *ConfigFilePath))
+			{
+				GLog->Logf(ELogVerbosity::Display, TEXT("Saved updated Max properties to %s"), *ConfigFilePath);
+			}
+			else
+			{
+				GLog->Logf(ELogVerbosity::Warning, TEXT("Failed to write config file %s"), *ConfigFilePath);
+			}
+		}
+		else
+		{
+			GLog->Logf(ELogVerbosity::Warning, TEXT("Failed to serialize JSON for saving"));
+		}
+
+		GLog->Logf(ELogVerbosity::Display, TEXT("AdjustMaxPropertiesFromConfig: Completed processing all AIControllers"));
+		GLog->Logf(ELogVerbosity::Display, TEXT("ExecuteAdjustment: Completed"));
 	}
 
-	GLog->Logf(ELogVerbosity::Display, TEXT("AdjustMaxPropertiesFromConfig: Completed processing all PlayerControllers"));
-
-    GLog->Logf(ELogVerbosity::Display, TEXT("ExecuteAdjustment: Completed"));
+	StartAdjustmentTimer();
 }
